c21b9049f42f6204436c00816b0262e17d974a60
crodas <c16rodagomez@hotmail.com>
2023-07-06 22:49:52 -0400

    borrando slash

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const BASE_API = 'https://apidjango-mn8h.onrender.com';
 //export const BASE_API = 'http://localhost:8000';
-export const LOCAL_API = 'https://parador-liza.netlify.app/';
+export const LOCAL_API = 'https://parador-liza.netlify.app';
 export const UPLOAD_IMAGE ='https://api.imgbb.com/1/upload'
 export const IMAGE_API_KEY ='cdbcfb73ad10c21ef13c63e944550112'
 export const TOKEN = 'token';
